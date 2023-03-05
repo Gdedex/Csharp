b1b62213429235b793ed53555acaf8697cb4b5e8
Gabriel Silva <112996165+Gdedex@users.noreply.github.com>
2023-03-05 15:16:30 -0300

    Add files via upload

---
+++ Triangulos/Program.cs
@@ -0,0 +1,28 @@
+﻿int lado1, lado2, lado3;
+
+Console.WriteLine("CALCULADORA DE TRIANGULOS");
+Console.WriteLine();
+
+Console.Write("Valor do lado 1: ");
+lado1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor do lado 2: ");
+lado2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor do lado 3: ");
+lado3 = Convert.ToInt32(Console.ReadLine());
+
+if (lado1 > lado2 + lado3 || lado2 > lado1 + lado3 || lado3 > lado1 + lado2){
+    Console.WriteLine("Não pode ser um triângulo");
+}
+else if (lado1 == lado2 && lado2 == lado3){
+    Console.WriteLine("É um triângulo Equilatero, pois todos os seus lado possuem a mesma medida");
+}
+else if (lado1 == lado2 || lado2 == lado3)
+{
+    Console.WriteLine("é um Triangulo Isóseles, pois dois lados pussuem a mesma medida");
+}
+else
+{
+    Console.WriteLine("É um triângulo Escaleno, pois todos os lados possuem medidas destintas");
+}
