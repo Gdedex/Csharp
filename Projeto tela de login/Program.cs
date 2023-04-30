5f4c63679b364b55f5b02f635b10296c00f154ea
Gabriel Silva <112996165+Gdedex@users.noreply.github.com>
2023-04-30 13:27:10 -0300

    Add files via upload

---
+++ Projeto tela de login/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_tela_de_login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("DIGITE O USUÁRIO: ");
+                string usuario = Console.ReadLine();
+                Console.WriteLine("DIGITE SUA SENHA: ");
+                string senha = "";
+                while (true)
+                {
+                    ConsoleKeyInfo tecla = Console.ReadKey(true); //NÃO MOSTRA A SENHA DIGITADA PELA USUÁRIO
+                    if (tecla.Key == ConsoleKey.Enter) //TERMINA O LOOP CASO O ENTER SEJA PRESSIONADO
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        senha += tecla.KeyChar; //JUNTA A TECLA DIGITADA A SENHA
+                    }
+                }
+                if (usuario == "gabriel" && senha == "1234") //LOGIN DE ACESSO USER = GABRIEL / SENHA = 1234
+                {
+                    Console.WriteLine("Login realizado com sucesso!");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Usuário ou senha incorretos");
+                    Console.WriteLine("Tente novamente");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
